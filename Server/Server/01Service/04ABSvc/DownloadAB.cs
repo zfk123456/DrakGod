ed301dcde210b5b8384056e1d89743fd7a7783cd
zfk <874354214@qq.com>
2024-01-08 22:39:08 +0800

    no message

---
+++ Server/Server/01Service/04ABSvc/DownloadAB.cs
@@ -0,0 +1,43 @@
+﻿/****************************************************
+	文件：NetSvc.cs 	
+	功能：AB包服务
+*****************************************************/
+using System;
+using System.IO;
+using PENet;
+using PEProtocol;
+
+//public class DownloadAB
+//{
+
+//    public void ABDownload(ClientState state, string abName, string abVersion)
+//    {
+//        // 构造AB包的完整路径 ABFolder为存放ab包的文件夹名
+//        //Directory.GetCurrentDirectory获取应用程序当前工作目录
+//        // Path.Combine获取包含当前执行的代码的程序集的加载文件的完整路径
+//        string abPath = Path.Combine(Directory.GetCurrentDirectory(), "ABFolder", abName + "." + abVersion);
+
+//        // 判断AB包是否存在
+//        if (File.Exists(abPath))
+//        {
+//            // 读取AB包内容，并发送给客户端
+//            byte[] abBytes = File.ReadAllBytes(abPath);
+//            byte[] sendData = Message.PackData((int)MessageCode.DownloadAB, abBytes);
+//            state.socket.Send(sendData);
+//        }
+//        else
+//        {
+//            // AB包不存在，返回错误信息给客户端
+//            byte[] sendData = Message.PackData((int)MessageCode.Error, "AB package not found.");
+//            state.socket.Send(sendData);
+//        }
+//    }
+
+//    public void ReqABDownlod(MsgPack pack)
+//    {
+
+//        ReqABDownlod data = pack.msg.reqABDownlod;//转接当前客户端的ab请求下载信息（获取当前判断是否需要下载的一个依据 如服务器设置一个唯一静态变量版本号 则游戏加载时就发送网络消息过来 判断当前版本是否与服务器中存储的版本号相同）
+
+//    }
+//}
+
---
+++ Server/Server/02System/10VersionSys/VersionSys.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using PEProtocol;
+
+public class VersionSys
+{
+    private static VersionSys instance = null;
+    public static VersionSys Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new VersionSys();
+            }
+            return instance;
+        }
+    }
+    public void Init()
+    {
+
+        PECommon.Log("VersionSys Init Done.");
+    }
+
+    public void ReqABDownlod(MsgPack pack)
+    {
+        //接收客户端传来的版本号信息
+        ReqABDownlod data = pack.msg.reqABDownlod;
+        //设定当前服务器发送的网络消息cmd
+        GameMsg msg = new GameMsg
+        {
+            cmd = (int)CMD.RspABDownlod,
+        };
+        //比较服务器当前版本号与客户端当前版本号是否相同
+        //由于当前服务器逻辑较为简单 也没有部署socket热部署程序集  所以当前简单逻辑为服务器的版本号只要与客户端的不相同就代表有更新
+        if (data.VersionNumber !=PECommon.clientVersion)
+        {
+            RspABDownlod rspABDownlod = new RspABDownlod
+            {
+                VersionNumber = PECommon.clientVersion,//发送当前服务器版本号给客户端
+                //同时发送下载的url  最好封装一个下载类   将当前服务器版本号传入然后返回一个对应的url返回给客户端 客户端根据对应url下载
+                IsNeedABDownlod = true,
+            };
+            msg.rspABDownlod = rspABDownlod;
+        }
+        else if(data.VersionNumber== PECommon.clientVersion)
+        {
+            RspABDownlod rspABDownlod = new RspABDownlod
+            {
+            
+                IsNeedABDownlod = false,
+            };
+            msg.rspABDownlod = rspABDownlod;
+        }
+        else
+        {
+            //抛出异常给客户端
+        }
+        pack.session.SendMsg(msg);//发送网络消息给客户端
+
+    }
+}
